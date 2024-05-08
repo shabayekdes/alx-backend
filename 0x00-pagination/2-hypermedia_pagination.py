39a2c94ee8de8c1864ac200e31f81ac4611be3c6
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-05-09 00:17:41 +0300

    Add task 2

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""Task 2: Hypermedia pagination
+"""
+
+import csv
+import math
+from typing import Dict, List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def _load_dataset(self) -> List[List]:
+        """Load dataset from CSV file."""
+        if self.__dataset is None:
+            try:
+                with open(self.DATA_FILE) as f:
+                    reader = csv.reader(f)
+                    self.__dataset = [row for row in reader][1:]  # Skip header
+            except FileNotFoundError:
+                print(f"Error: File '{self.DATA_FILE}' not found.")
+                self.__dataset = []
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data."""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        data = self._load_dataset()
+        total_records = len(data)
+        total_pages = math.ceil(total_records / page_size)
+
+        if page > total_pages:
+            return []
+
+        start, end = index_range(page, page_size)
+        return data[start:end]
+
+    def total_pages(self, page_size: int = 10) -> int:
+        """Calculates the total number of pages."""
+        data = self._load_dataset()
+        total_records = len(data)
+        return math.ceil(total_records / page_size)
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Retrieves information about a page.
+        """
+        data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': math.ceil(len(self.__dataset) / page_size)
+        }
