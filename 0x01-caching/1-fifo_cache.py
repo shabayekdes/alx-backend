d78dfd83899406a5369d30bc1f943a4785fd90e1
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-05-11 23:23:45 +0300

    Add task 2

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Task 1: FIFO caching
+"""
+
+
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """A class FIFOCache that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """assign to the dictionary cache_data
+        """
+
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in cache_data
+        """
+
+        return self.cache_data.get(key, None)
