d7a8fb86312d64e55d9e5d93ecf5eb34517fc7c6
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-05-11 23:57:26 +0300

    fix

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,98 +1,73 @@
 #!/usr/bin/env python3
 """Task 5: Least Frequently Used caching module.
 """
-from collections import defaultdict
+from collections import OrderedDict
+
 from base_caching import BaseCaching
 
+
 class LFUCache(BaseCaching):
-    """Least Frequently Used (LFU) caching implementation.
-
-    Inherits from BaseCaching and provides a caching system based on the LFU eviction policy.
-
-    Attributes:
-        cache_data (dict): Dictionary to store key-value pairs of cached items.
-        freq_map (defaultdict): Dictionary to map item frequencies to lists of keys with that frequency.
-        key_freq (dict): Dictionary to track the current frequency of each cached key.
-        min_freq (int): Minimum frequency among all cached items.
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LFU
+    removal mechanism when the limit is reached.
     """
-
     def __init__(self):
-        """Initializes the LFU cache.
-
-        Overrides the BaseCaching class constructor to set up LFU-specific attributes.
+        """Initializes the cache.
         """
         super().__init__()
-        self.freq_map = defaultdict(list)
-        self.key_freq = {}
-        self.min_freq = 0
+        self.cache_data = OrderedDict()
+        self.keys_freq = []
 
-    def _update_frequency(self, key):
-        """Updates the frequency of a cached key.
-
-        Args:
-            key: The key whose frequency needs to be updated.
+    def __reorder_items(self, mru_key):
+        """Reorders the items in this cache based on the most
+        recently used item.
         """
-        freq = self.key_freq.get(key, 0)
-        new_freq = freq + 1
-        self.key_freq[key] = new_freq
-        self.freq_map[new_freq].append(key)
-        if freq > 0:
-            self.freq_map[freq].remove(key)
-            if not self.freq_map[freq]:  # If no keys left at this frequency
-                if freq == self.min_freq:
-                    self.min_freq += 1
+        max_positions = []
+        mru_freq = 0
+        mru_pos = 0
+        ins_pos = 0
+        for i, key_freq in enumerate(self.keys_freq):
+            if key_freq[0] == mru_key:
+                mru_freq = key_freq[1] + 1
+                mru_pos = i
+                break
+            elif len(max_positions) == 0:
+                max_positions.append(i)
+            elif key_freq[1] < self.keys_freq[max_positions[-1]][1]:
+                max_positions.append(i)
+        max_positions.reverse()
+        for pos in max_positions:
+            if self.keys_freq[pos][1] > mru_freq:
+                break
+            ins_pos = pos
+        self.keys_freq.pop(mru_pos)
+        self.keys_freq.insert(ins_pos, [mru_key, mru_freq])
 
     def put(self, key, item):
-        """Adds or updates an item in the LFU cache.
-
-        If the cache size exceeds the maximum allowed items, it evicts the least frequently used item(s).
-
-        Args:
-            key: The key of the item to be added or updated.
-            item: The value of the item to be added or updated.
-
-        If key or item is None, this method does nothing.
+        """Adds an item in the cache.
         """
         if key is None or item is None:
             return
-        if key in self.cache_data:
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lfu_key, _ = self.keys_freq[-1]
+                self.cache_data.pop(lfu_key)
+                self.keys_freq.pop()
+                print("DISCARD:", lfu_key)
             self.cache_data[key] = item
-            self._update_frequency(key)
+            ins_index = len(self.keys_freq)
+            for i, key_freq in enumerate(self.keys_freq):
+                if key_freq[1] == 0:
+                    ins_index = i
+                    break
+            self.keys_freq.insert(ins_index, [key, 0])
         else:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # Evict least frequency used item
-                while self.min_freq not in self.freq_map or not self.freq_map[self.min_freq]:
-                    self.min_freq += 1
-                lfu_key = self.freq_map[self.min_freq][0]
-                del self.cache_data[lfu_key]
-                self.freq_map[self.min_freq].pop(0)
-                if not self.freq_map[self.min_freq]:  # If no keys left at this frequency
-                    self.min_freq += 1
-                print(f"DISCARD: {lfu_key}")
             self.cache_data[key] = item
-            self._update_frequency(key)
+            self.__reorder_items(key)
 
     def get(self, key):
-        """Retrieves an item from the LFU cache.
-
-        If the requested key exists in the cache, its frequency is updated.
-
-        Args:
-            key: The key of the item to be retrieved.
-
-        Returns:
-            The value associated with the specified key, or None if the key is not found.
+        """Retrieves an item by key.
         """
-        if key is None or key not in self.cache_data:
-            return None
-        self._update_frequency(key)
-        return self.cache_data[key]
-
-    def print_cache(self):
-        """Prints the current state of the LFU cache.
-
-        Displays the key-value pairs stored in the cache.
-        """
-        print("Current cache:")
-        for key in self.cache_data:
-            print(f"{key}: {self.cache_data[key]}")
+        if key is not None and key in self.cache_data:
+            self.__reorder_items(key)
+        return self.cache_data.get(key, None)
